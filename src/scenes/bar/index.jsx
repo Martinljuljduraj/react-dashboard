636c2227dc8748d8cadab0111b49fa59476d482b
Martin Lulgjuraj <martinlulgjuraj@mac.mynetworksettings.com>
2025-05-21 01:26:15 -0400

    changed mock data

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -4,7 +4,7 @@ import { Header, BarChart } from "../../components";
 const Bar = () => {
   return (
     <Box m="20px">
-      <Header title="Bar Chart" subtitle="Simple Bar Chart" />
+      <Header title="Bar Chart" subtitle="The foods people wanna eat too much of:" />
       <Box height="75vh">
         <BarChart />
       </Box>
--- src/scenes/line/index.jsx
+++ src/scenes/line/index.jsx
@@ -4,8 +4,8 @@ import { Header, LineChart } from "../../components";
 const Line = () => {
   return (
     <Box m="20px">
-      <Header title="Line Chart" subtitle="Simple Line Chart" />
-      <Box height="75vh">
+      <Header title="Line Chart" subtitle="Displays which methods of transportation people use in various countries" />
+      <Box height="100vh">
         <LineChart />
       </Box>
     </Box>
